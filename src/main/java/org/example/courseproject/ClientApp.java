49ebd0919b899ba1baa07fde906a693f009f661b
Ekaterina Shimchyonok <shimchyonokat@gmail.com>
2024-10-15 21:10:11 +0300

    Added registration

---
+++ src/main/java/org/example/courseproject/ClientApp.java
@@ -0,0 +1,28 @@
+package org.example.courseproject;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+
+
+public class ClientApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/registration.fxml"));
+            primaryStage.setTitle("Registration");
+            primaryStage.setScene(new Scene(root, 300, 200));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
